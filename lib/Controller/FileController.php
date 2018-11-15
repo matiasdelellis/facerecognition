f2c53dd05da00d6a0d8d89939cdfb6a21443513b
Matias De lellis <mati86dl@gmail.com>
2018-11-14 21:00:08 -0300

    Consider the model check the response mapper.

--- lib/Controller/FileController.php
+++ lib/Controller/FileController.php
@@ -2,6 +2,7 @@
 namespace OCA\FaceRecognition\Controller;
 
 use OCP\IRequest;
+use OCP\IConfig;
 use OCP\Files\IRootFolder;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
@@ -15,7 +16,11 @@ use OCA\FaceRecognition\Db\FaceNewMapper;
 use OCA\FaceRecognition\Db\Person;
 use OCA\FaceRecognition\Db\PersonMapper;
 
+use OCA\FaceRecognition\Migration\AddDefaultFaceModel;
+
 class FileController extends Controller {
+
+	private $config;
 
 	private $personMapper;
 
@@ -25,13 +30,14 @@ class FileController extends Controller {
 
 	private $userId;
 
-	public function __construct($AppName, IRequest $request,
+	public function __construct($AppName, IRequest $request, IConfig $config,
 	                            PersonMapper $personmapper,
 	                            FaceNewMapper $facenewmapper,
 	                            IRootFolder $rootFolder,
 	                            $UserId)
 	{
 		parent::__construct($AppName, $request);
+		$this->config = $config;
 		$this->personMapper = $personmapper;
 		$this->faceNewMapper = $facenewmapper;
 		$this->rootFolder = $rootFolder;
@@ -42,17 +48,24 @@ class FileController extends Controller {
 	 * @NoAdminRequired
 	 */
 	public function getPersonsFromPath(string $fullpath) {
+		$model = intval($this->config->getAppValue('facerecognition', 'model', AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID));
+
 		$userFolder = $this->rootFolder->getUserFolder($this->userId);
 		$fileId = $userFolder->get($fullpath)->getId();
 
 		$resp = array();
 		$persons = $this->personMapper->findFromFile($this->userId, $fileId);
 		foreach ($persons as $person) {
-			$face = array();
-			$face['name'] = $person->getName();
-			$face['person_id'] = $person->getId();
-			$face['face'] = $this->faceNewMapper->getPersonOnFile($this->userId, $person->getId(), $fileId, 1)[0];
-			$resp[] = $face;
+			$face = $this->faceNewMapper->getPersonOnFile($this->userId, $person->getId(), $fileId, $model);
+			if (!count($face))
+				continue;
+
+			$facePerson = array();
+			$facePerson['name'] = $person->getName();
+			$facePerson['person_id'] = $person->getId();
+			$facePerson['face'] = $face[0];
+
+			$resp[] = $facePerson;
 		}
 		return new DataResponse($resp);
 	}

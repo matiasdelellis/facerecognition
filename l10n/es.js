7b0a8856c6fb2bdbc48647a98a68661875ba50c5
Matias De lellis <mati86dl@gmail.com>
2018-12-12 21:54:18 -0300

    Fix typos on ES translation.

--- l10n/es.js
+++ l10n/es.js
@@ -6,12 +6,12 @@ OC.L10N.register(
     "Please enter a name to rename the person" : "Por favor ingrese un nombre para renombrar la persona",
     "Rename" : "Renombrar",
     "Rename Person" : "Renombrar Persona",
-    "Cancel" : "Cancelae",
+    "Cancel" : "Cancelar",
     "Face Recognition" : "Reconocimiento Facial",
-    "A face recognition app" : "A aplicacion de reconocimiento facial",
+    "A face recognition app" : "Una aplicacion de reconocimiento facial",
     "Open Documentation" : "Abrir Documentación",
-    "Configuration information" : "Información de la configuracion",
+    "Configuration information" : "Información de la configuración",
     "Here you can see the photos of your friends that we have recognized" : "Aqui puedes ver las fotos de tus amigos que reconocimos",
-    "Looking for your recognized friends" : "Buscando tus archicos reconocidos"
+    "Looking for your recognized friends" : "Buscando tus amigos reconocidos"
 },
 "");

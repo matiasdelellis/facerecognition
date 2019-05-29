85089d28073ef858888185865165853f50a23729
Matias De lellis <mati86dl@gmail.com>
2019-05-29 12:22:33 -0300

    Don't abort when memory setting exceeds system/PHP memory.
    Better var name and message.

--- lib/BackgroundJob/Tasks/CheckRequirementsTask.php
+++ lib/BackgroundJob/Tasks/CheckRequirementsTask.php
@@ -78,55 +78,53 @@ class CheckRequirementsTask extends FaceRecognitionBackgroundTask {
 		}
 
 		// Determine the system memory with some considerations.
-		$memory = MemoryLimits::getAvailableMemory();
-		if ($memory <= 0) {
+		$memoryAvailable = MemoryLimits::getAvailableMemory();
+		if ($memoryAvailable <= 0) {
 			// We cannot determine amount of memory to give to face recognition CNN.
 			// We will hardcode it here to 1GB, but plan is to expose this to user in future.
 			// TODO: allow user to choose "recognition quality", which will map to given memory.
 			// If user explicitely set something, we ignore getting memory from system.
 			$this->logDebug("Cannot detect amount of memory given to PHP process. Using 1GB for image processing");
-			$memory = 1024 * 1024 * 1024;
+			$memoryAvailable = 1024 * 1024 * 1024;
+		}
 
 		// Apply some prudent limits.
-		if ($memory < 1024 * 1024 * 1024) {
+		if ($memoryAvailable < 1024 * 1024 * 1024) {
 			$error_message =
 				"\n" .
-				"Seems that you have only " . intval($memory / (1024 * 1024)). "MB of memory given to PHP.\n" .
+				"Seems that you have only " . intval($memoryAvailable / (1024 * 1024)). "MB of memory given to PHP.\n" .
 				"Face recognition application requires at least 1 GB. You need to change your memory_limit in php.ini.\n" .
 				"Check https://secure.php.net/manual/en/ini.core.php#ini.memory-limit for details.\n" .
 				"If you already set this to unlimited, it seems your system is not having enough RAM memory.";
 			$this->logInfo($error_message);
 			return false;
 		} else {
-			$this->logDebug(sprintf('Found %d MB available to PHP.', $memory / (1024 * 1024)));
+			$this->logDebug(sprintf('Found %d MB available to PHP.', $systemMemory / (1024 * 1024)));
 			// No point going above 4GB ("4GB should be enough for everyone")
-			if ($memory > 4 * 1024 * 1024 * 1024) {
+			if ($memoryAvailable > 4 * 1024 * 1024 * 1024) {
 				$this->logDebug('Capping used memory to maximum of 4GB');
-				$memory = 4 * 1024 * 1024 * 1024;
+				$memoryAvailable = 4 * 1024 * 1024 * 1024;
 			}
 		}
 
 		// Apply admin setting limit.
 		$memorySetting = $this->config->getAppValue('facerecognition', 'memory-limits', '-1');
 		if ($memorySetting > 0) {
-			if ($memorySetting > $memory) {
+			if ($memorySetting > $memoryAvailable) {
 				$error_message =
 					"\n" .
-					"Seems that you configured" . intval($memorySetting / (1024 * 1024)) . " MB of memory, however, this exede"
-					"detected as maximum for PHP: " . intval($memory / (1024 * 1024)). "MB.\n" .
-					"You need to change your memory_limit in php.ini or 'Memory Limits' in settings.\n" .
-					"Check https://secure.php.net/manual/en/ini.core.php#ini.memory-limit for details.\n" .
-					"If you already set this to unlimited, it seems your system is not having enough RAM memory.";
+					"Seems that you configured" . intval($memorySetting / (1024 * 1024)) . " MB of memory, however, this exceeds"
+					"detected as maximum for PHP: " . intval($memoryAvailable / (1024 * 1024)). "MB.\n" .
+					"We will ignore the FaceRecognition settings, and we will limit to that.";
 				$this->logInfo($error_message);
-				return false;
 			}
 			else {
 				$this->logInfo("Applying the memory limit to " . intval($memorySetting / (1024*1024)) . "MB configured in settings.");
-				$memory = $memorySetting;
+				$memoryAvailable = $memorySetting;
 			}
 		}
 
-		$context->propertyBag['memory'] = $memory;
+		$context->propertyBag['memory'] = $memoryAvailable;
 
 		return true;
 	}

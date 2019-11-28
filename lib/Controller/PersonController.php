06aff49d398776ab5bbab097930e0678f4cb0d1a
Matias De lellis <mati86dl@gmail.com>
2019-11-28 10:51:52 -0300

    Add button to show all clusters with same person name

--- lib/Controller/PersonController.php
+++ lib/Controller/PersonController.php
@@ -70,40 +70,41 @@ class PersonController extends Controller {
 		$resp['enabled'] = $userEnabled;
 		$resp['clusters'] = array();
 
-		if ($userEnabled === 'true') {
-			$persons = $this->personMapper->findAll($this->userId);
-			foreach ($persons as $person) {
-				$personFaces = $this->faceMapper->findFacesFromPerson($this->userId, $person->getId(), $model);
-				if ($notGrouped === 'false' && count($personFaces) <= 1)
+		if ($userEnabled !== 'true')
+			return new DataResponse($resp);
+
+		$persons = $this->personMapper->findAll($this->userId);
+		foreach ($persons as $person) {
+			$personFaces = $this->faceMapper->findFacesFromPerson($this->userId, $person->getId(), $model);
+			if ($notGrouped === 'false' && count($personFaces) <= 1)
+				continue;
+
+			$limit = 14;
+			$faces = [];
+			foreach ($personFaces as $personFace) {
+				if ($limit-- === 0)
+					break;
+
+				$image = $this->imageMapper->find($this->userId, $personFace->getImage());
+				$fileUrl = $this->getRedirectToFileUrl($image->getFile());
+				if (NULL === $fileUrl) {
+					$limit++;
 					continue;
-
-				$limit = 14;
-				$faces = [];
-				foreach ($personFaces as $personFace) {
-					if ($limit-- === 0)
-						break;
-
-					$image = $this->imageMapper->find($this->userId, $personFace->getImage());
-					$fileUrl = $this->getRedirectToFileUrl($image->getFile());
-					if (NULL === $fileUrl) {
-						$limit++;
-						continue;
-					}
-
-					$face = [];
-					$face['thumb-url'] = $this->getThumbUrl($personFace->getId());
-					$face['file-url'] = $fileUrl;
-					$faces[] = $face;
 				}
 
-				$cluster = [];
-				$cluster['name'] = $person->getName();
-				$cluster['count'] = count($personFaces);
-				$cluster['id'] = $person->getId();
-				$cluster['faces'] = $faces;
-
-				$resp['clusters'][] = $cluster;
+				$face = [];
+				$face['thumb-url'] = $this->getThumbUrl($personFace->getId());
+				$face['file-url'] = $fileUrl;
+				$faces[] = $face;
 			}
+
+			$cluster = [];
+			$cluster['name'] = $person->getName();
+			$cluster['count'] = count($personFaces);
+			$cluster['id'] = $person->getId();
+			$cluster['faces'] = $faces;
+
+			$resp['clusters'][] = $cluster;
 		}
 		return new DataResponse($resp);
 	}
@@ -111,7 +112,7 @@ class PersonController extends Controller {
 	/**
 	 * @NoAdminRequired
 	 */
-	public function find($id) {
+	public function find(int $id) {
 		$model = intval($this->config->getAppValue('facerecognition', 'model', AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID));
 
 		$person = $this->personMapper->find($this->userId, $id);
@@ -133,6 +134,50 @@ class PersonController extends Controller {
 		$resp['id'] = $person->getId();
 		$resp['faces'] = $faces;
 
+		return new DataResponse($resp);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 */
+	public function findByName(string $personName) {
+		$model = intval($this->config->getAppValue('facerecognition', 'model', AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID));
+		$notGrouped = $this->config->getAppValue('facerecognition', 'show-not-grouped', 'false');
+		$userEnabled = $this->config->getUserValue($this->userId, 'facerecognition', 'enabled', 'false');
+
+		$resp = array();
+		$resp['enabled'] = $userEnabled;
+		$resp['clusters'] = array();
+
+		if ($userEnabled !== 'true')
+			return new DataResponse($resp);
+
+		$persons = $this->personMapper->findByName($this->userId, $personName);
+		foreach ($persons as $person) {
+			$personFaces = $this->faceMapper->findFacesFromPerson($this->userId, $person->getId(), $model);
+			if ($notGrouped === 'false' && count($personFaces) <= 1)
+				continue;
+
+			$faces = [];
+			foreach ($personFaces as $personFace) {
+				$image = $this->imageMapper->find($this->userId, $personFace->getImage());
+				$fileUrl = $this->getRedirectToFileUrl($image->getFile());
+				if (NULL === $fileUrl)
+					continue;
+
+				$face = [];
+				$face['thumb-url'] = $this->getThumbUrl($personFace->getId());
+				$face['file-url'] = $fileUrl;
+				$faces[] = $face;
+			}
+
+			$cluster = [];
+			$cluster['name'] = $person->getName();
+			$cluster['count'] = count($personFaces);
+			$cluster['id'] = $person->getId();
+			$cluster['faces'] = $faces;
+			$resp['clusters'][] = $cluster;
+		}
 		return new DataResponse($resp);
 	}
 

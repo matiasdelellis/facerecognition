e151d23cf4269007baf4a2da82813dd8feb9aeb0
Matias De lellis <mati86dl@gmail.com>
2019-12-03 10:52:14 -0300

    Test that is installed default model

--- tests/unit/RequirementsTest.php
+++ tests/unit/RequirementsTest.php
@@ -56,6 +56,13 @@ class RequirementsTest extends TestCase {
 		$this->context->logger = new FaceRecognitionLogger($logger);
 	}
 
+	public function testModelInstalled() {
+		$config = $this->createMock(IConfig::class);
+		$defaultModel = AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID;
+		$modelInstalled = intval($config->getAppValue('facerecognition', 'model', AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID));
+		$this->assertEquals($defaultModel, $modelInstalled);
+	}
+
 	public function testPdlibLoaded() {
 		$requirements = new Requirements($this->context->modelService, AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID);
 		$this->assertTrue($requirements->pdlibLoaded());

4714c57d850ec8a357c1d626d116f13467acebed
Matias De lellis <mati86dl@gmail.com>
2019-12-04 11:43:32 -0300

    ls everywhere ..

--- tests/travis-script.sh
+++ tests/travis-script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+ls -alR /home/travis/build/matiasdelellis/server/data-autotest
 ../../occ face:setup
+ls -alR /home/travis/build/matiasdelellis/server/data-autotest
 make test
+ls -alR /home/travis/build/matiasdelellis/server/data-autotest
 
 # Create coverage report
 wget https://scrutinizer-ci.com/ocular.phar

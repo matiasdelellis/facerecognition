66dab0b3a023bcedec9b1907932ee9853f062233
Matias De lellis <mati86dl@gmail.com>
2020-01-22 22:10:36 -0300

    Move Search Provider to SettingsService

--- lib/Search/Provider.php
+++ lib/Search/Provider.php
@@ -1,36 +1,51 @@
 <?php
+declare(strict_types=1);
 /**
- * Face Recognition
+ * @copyright Copyright (c) 2017, 2018, 2020 Matias De lellis <mati86dl@gmail.com>
  *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the LICENSE.md file.
+ * @author Matias De lellis <mati86dl@gmail.com>
  *
- * @copyright 2017 Matias De lellis <mati86dl@delellis.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 namespace OCA\FaceRecognition\Search;
+
 use OCA\FaceRecognition\AppInfo\Application;
-use OCA\FaceRecognition\Migration\AddDefaultFaceModel;
+
+use OCA\FaceRecognition\Service\SettingsService;
 
 /**
  * Provide search results from the 'facerecognition' app
  */
 class Provider extends \OCP\Search\Provider {
 
+	/** @var ImageMapper Image mapper */
 	private $imageMapper;
 
-	private $config;
-
-	//private $l10N;
+	/** @var SettingsService Settings service */
+	private $settigsService;
 
 	public function __construct() {
 		$app = new Application();
 		$container = $app->getContainer();
 
-		$this->app = $app;
-		$this->config = $container->query('OCP\IConfig');
-		$this->imageMapper = $container->query(\OCA\FaceRecognition\Db\ImageMapper::class);
-		//$this->l10n = $container->query('L10N');
+		$this->app             = $app;
+		$this->imageMapper     = $container->query(\OCA\FaceRecognition\Db\ImageMapper::class);
+		$this->settingsService = $container->query(\OCA\FaceRecognition\Service\SettingsService::class);
 	}
 
 	/**
@@ -43,7 +58,7 @@ class Provider extends \OCP\Search\Provider {
 		$userId = \OC::$server->getUserSession()->getUser()->getUID();
 		$ownerView = new \OC\Files\View('/'. $userId . '/files');
 
-		$model = intval($this->config->getAppValue('facerecognition', 'model', AddDefaultFaceModel::DEFAULT_FACE_MODEL_ID));
+		$model = $this->settingsService->getCurrentFaceModel();
 
 		$searchresults = array();
 

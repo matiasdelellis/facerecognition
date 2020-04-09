cc163e74f1d9546d4cd809f442938835af8883e6
Matias De lellis <mati86dl@gmail.com>
2020-04-09 19:58:36 -0300

    Use references to get a description of the problem. Issue #244

--- lib/Model/DlibHogModel/DlibHogModel.php
+++ lib/Model/DlibHogModel/DlibHogModel.php
@@ -125,10 +125,20 @@ class DlibHogModel implements IModel {
 		return true;
 	}
 
-	public function meetDependencies(): bool {
-		return ((extension_loaded('pdlib')) &&
-		        (version_compare(phpversion('pdlib'), '1.0.1', '>=')) &&
-		        (MemoryLimits::getAvailableMemory() >= static::MINIMUM_MEMORY_REQUIREMENTS));
+	public function meetDependencies(string &$error_message): bool {
+		if (!extension_loaded('pdlib')) {
+			$error_message = "The PDlib PHP extension is not loaded";
+			return false;
+		}
+		if (!version_compare(phpversion('pdlib'), '1.0.1', '>=')) {
+			$error_message = "The PDlib PHP extension version is too old";
+			return false;
+		}
+		if (MemoryLimits::getAvailableMemory() < static::MINIMUM_MEMORY_REQUIREMENTS) {
+			$error_message = "Your system does not meet the minimum memory requirements";
+			return false;
+		}
+		return true;
 	}
 
 	public function getMaximumArea(): int {

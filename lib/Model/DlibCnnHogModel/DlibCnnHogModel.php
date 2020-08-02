055b02f5cd38d34fde563cc2f89f036cd421eac8
Matias De lellis <mati86dl@gmail.com>
2020-08-02 19:40:59 -0300

    Reuse default model.. Since it is just an improvement on this.

--- lib/Model/DlibCnnHogModel/DlibCnnHogModel.php
+++ lib/Model/DlibCnnHogModel/DlibCnnHogModel.php
@@ -25,13 +25,10 @@ namespace OCA\FaceRecognition\Model\DlibCnnHogModel;
 
 use OCP\IDBConnection;
 
-use OCA\FaceRecognition\Helper\MemoryLimits;
-
-use OCA\FaceRecognition\Service\FileService;
-use OCA\FaceRecognition\Service\ModelService;
-use OCA\FaceRecognition\Service\SettingsService;
-
 use OCA\FaceRecognition\Model\IModel;
+
+use OCA\FaceRecognition\Model\DlibCnnModel\DlibCnn5Model;
+use OCA\FaceRecognition\Model\DlibHogModel\DlibHogModel;
 
 class DlibCnnHogModel implements IModel {
 
@@ -43,67 +40,29 @@ class DlibCnnHogModel implements IModel {
 	const FACE_MODEL_DESC = "Default Cnn model with Hog validation, and 5 point landmarks preprictor";
 	const FACE_MODEL_DOC = "";
 
-	/** Relationship between image size and memory consumed */
-	const MEMORY_AREA_RELATIONSHIP = 1 * 1024;
-	const MINIMUM_MEMORY_REQUIREMENTS = 1 * 1024 * 1024 * 1024;
-
-	const FACE_MODEL_BZ2_URLS = [
-		'https://github.com/davisking/dlib-models/raw/94cdb1e40b1c29c0bfcaf7355614bfe6da19460e/mmod_human_face_detector.dat.bz2',
-		'https://github.com/davisking/dlib-models/raw/4af9b776281dd7d6e2e30d4a2d40458b1e254e40/shape_predictor_5_face_landmarks.dat.bz2',
-		'https://github.com/davisking/dlib-models/raw/2a61575dd45d818271c085ff8cd747613a48f20d/dlib_face_recognition_resnet_model_v1.dat.bz2'
-	];
-
-	const FACE_MODEL_FILES = [
-		'mmod_human_face_detector.dat',
-		'shape_predictor_5_face_landmarks.dat',
-		'dlib_face_recognition_resnet_model_v1.dat'
-	];
-
-	const I_MODEL_DETECTOR = 0;
-	const I_MODEL_PREDICTOR = 1;
-	const I_MODEL_RESNET = 2;
-
-	const PREFERRED_MIMETYPE = 'image/png';
-
-	/** @var \CnnFaceDetection */
-	private $cfd;
-
-	/** @var \FaceLandmarkDetection */
-	private $fld;
-
-	/** @var \FaceRecognition */
-	private $fr;
-
 	/** @var IDBConnection */
 	private $connection;
 
-	/** @var FileService */
-	private $fileService;
+	/** @var DlibCnn5Model */
+	private $dlibCnn5Model;
 
-	/** @var ModelService */
-	private $modelService;
-
-	/** @var SettingsService */
-	private $settingsService;
-
+	/** @var DlibHogModel */
+	private $dlibHogModel;
 
 	/**
-	 * DlibCnnModel __construct.
+	 * DlibCnnHogModel __construct.
 	 *
 	 * @param IDBConnection $connection
-	 * @param FileService $fileService
-	 * @param ModelService $modelService
-	 * @param SettingsService $settingsService
+	 * @param DlibCnn5Model $dlibCnn5Model
+	 * @param DlibHogModel $dlibHogModel
 	 */
 	public function __construct(IDBConnection   $connection,
-	                            FileService     $fileService,
-	                            ModelService    $modelService,
-	                            SettingsService $settingsService)
+	                            DlibCnn5Model   $dlibCnn5Model,
+	                            DlibHogModel    $dlibHogModel)
 	{
 		$this->connection       = $connection;
-		$this->fileService      = $fileService;
-		$this->modelService     = $modelService;
-		$this->settingsService  = $settingsService;
+		$this->dlibCnn5Model    = $dlibCnn5Model;
+		$this->dlibHogModel     = $dlibHogModel;
 	}
 
 	public function getId(): int {
@@ -123,59 +82,37 @@ class DlibCnnHogModel implements IModel {
 	}
 
 	public function isInstalled(): bool {
-		if (!$this->modelService->modelFileExists($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_DETECTOR]))
+		if (!$this->dlibCnn5Model->isInstalled())
 			return false;
-		if (!$this->modelService->modelFileExists($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_PREDICTOR]))
-			return false;
-		if (!$this->modelService->modelFileExists($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_RESNET]))
+		if (!$this->dlibHogModel->isInstalled())
 			return false;
 		return true;
 	}
 
 	public function meetDependencies(string &$error_message): bool {
-		if (!extension_loaded('pdlib')) {
-			$error_message = "The PDlib PHP extension is not loaded";
+		if (!$this->dlibCnn5Model->meetDependencies($error_message)) {
+			$error_message .= " This Model depend on Model 1 and must install it.";
 			return false;
 		}
-		if (!version_compare(phpversion('pdlib'), '1.0.1', '>=')) {
-			$error_message = "The PDlib PHP extension version is too old";
-			return false;
-		}
-		if (MemoryLimits::getAvailableMemory() < static::MINIMUM_MEMORY_REQUIREMENTS) {
-			$error_message = "Your system does not meet the minimum memory requirements";
+		if (!$this->dlibHogModel->meetDependencies($error_message)) {
+			$error_message .= " This Model depend on Model 3 and must install it.";
 			return false;
 		}
 		return true;
 	}
 
 	public function getMaximumArea(): int {
-		return intval(MemoryLimits::getAvailableMemory()/static::MEMORY_AREA_RELATIONSHIP);
+		return $this->dlibCnn5Model->getMaximumArea();
 	}
 
 	public function getPreferredMimeType(): string {
-		return static::PREFERRED_MIMETYPE;
+		return $this->dlibCnn5Model->getPreferredMimeType();
 	}
 
 	public function install() {
 		if ($this->isInstalled()) {
 			return;
 		}
-
-		// Create main folder where install models.
-		$this->modelService->prepareModelFolder($this->getId());
-
-		/* Download and install models */
-		$detectorModelBz2 = $this->fileService->downloaldFile(static::FACE_MODEL_BZ2_URLS[self::I_MODEL_DETECTOR]);
-		$this->fileService->bunzip2($detectorModelBz2, $this->modelService->getFileModelPath($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_DETECTOR]));
-
-		$predictorModelBz2 = $this->fileService->downloaldFile(static::FACE_MODEL_BZ2_URLS[self::I_MODEL_PREDICTOR]);
-		$this->fileService->bunzip2($predictorModelBz2, $this->modelService->getFileModelPath($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_PREDICTOR]));
-
-		$resnetModelBz2 = $this->fileService->downloaldFile(static::FACE_MODEL_BZ2_URLS[self::I_MODEL_RESNET]);
-		$this->fileService->bunzip2($resnetModelBz2, $this->modelService->getFileModelPath($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_RESNET]));
-
-		/* Clean temporary files */
-		$this->fileService->clean();
 
 		// Insert on database and enable it
 		$qb = $this->connection->getQueryBuilder();
@@ -200,16 +137,14 @@ class DlibCnnHogModel implements IModel {
 	}
 
 	public function open() {
-		$this->cfd = new \CnnFaceDetection($this->modelService->getFileModelPath($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_DETECTOR]));
-		$this->fld = new \FaceLandmarkDetection($this->modelService->getFileModelPath($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_PREDICTOR]));
-		$this->fr = new \FaceRecognition($this->modelService->getFileModelPath($this->getId(), static::FACE_MODEL_FILES[self::I_MODEL_RESNET]));
+		return $this->dlibCnn5Model->open();
 	}
 
 	public function detectFaces(string $imagePath): array {
 		$detectedFaces = [];
 
-		$cnnFaces = $this->cfd->detect($imagePath, 0);
-		$hogFaces = dlib_face_detection($imagePath);
+		$cnnFaces = $this->dlibCnn5Model->detectFaces($imagePath);
+		$hogFaces = $this->dlibHogModel->detectFaces($imagePath);
 
 		foreach ($cnnFaces as $proposedFace) {
 			$detectedFaces[] = $this->validateFace($proposedFace, $hogFaces);
@@ -219,11 +154,11 @@ class DlibCnnHogModel implements IModel {
 	}
 
 	public function detectLandmarks(string $imagePath, array $rect): array {
-		return $this->fld->detect($imagePath, $rect);
+		return $this->dlibCnn5Model->detectLandmarks($imagePath, $rect);
 	}
 
 	public function computeDescriptor(string $imagePath, array $landmarks): array {
-		return $this->fr->computeDescriptor($imagePath, $landmarks);
+		return $this->dlibCnn5Model->computeDescriptor($imagePath, $landmarks);
 	}
 
 	private function validateFace($proposedFace, $validateFaces) {

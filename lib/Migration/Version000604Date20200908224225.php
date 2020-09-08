5b741f4370cfa8ca6a286d2c963338904f23e6e1
Matias De lellis <mati86dl@gmail.com>
2020-09-08 20:45:23 -0300

    Bye bye 'New person..'

---
+++ lib/Migration/Version000604Date20200908224225.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\FaceRecognition\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version000604Date20200908224225 extends SimpleMigrationStep {
+
+	/** @var IDBConnection */
+	protected $connection;
+
+	public function __construct(IDBConnection $connection) {
+		$this->connection = $connection;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$personsTable = $schema->getTable('facerecog_persons');
+		$personsTable->changeColumn('name', [
+			'notnull' => false,
+			'default' => null,
+		]);
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		$query = $this->connection->getQueryBuilder();
+		$query->update('facerecog_persons')
+			->set('name', $query->createNamedParameter(null))
+			->where($query->expr()->iLike('name', 'New person %'));
+		$query->execute();
+	}
+
+}

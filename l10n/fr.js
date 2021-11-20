c84b4b61d1ad27e5beb5496e57c7503a80f1d4f6
Matias De lellis <mati86dl@gmail.com>
2021-11-20 13:24:18 -0300

    New Czech translation thanks to Pavel Borecki, and update others. Thank you so   much everyone.

--- l10n/fr.js
+++ l10n/fr.js
@@ -6,7 +6,7 @@ OC.L10N.register(
     "Analyzing images" : "Analyse des images en cours",
     "_%n image detected_::_%n images detected_" : ["%nimage détectée","%n images détectées"],
     "_%n image in queue_::_%n images in queue_" : ["%n image en attente","%n images en attente d'analyse"],
-    "Ends approximately {estimatedFinalize}" : "Se termine dans environ {estimatedFinalize}",
+    "Ends approximately {estimatedFinalize}" : "Se termine approximativement {estimatedFinalize}",
     "The analysis is not started yet" : "L'analyse n'a pas encore commencé",
     "The changes were saved. It will be taken into account in the next analysis." : "Les modifications ont été sauvegardées. Elles seront prises en compte lors de la prochaine analyse.",
     "The change could not be applied." : "La modification n'a pas pu être effectuée.",

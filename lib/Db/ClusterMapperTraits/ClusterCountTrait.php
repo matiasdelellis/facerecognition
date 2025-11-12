069979b8077a9fa18ce3fe52fbf86808f7315f00
Máté Zsólya <zsolyamate@gmail.com>
2025-11-12 20:14:16 +0100

    increase loglevel

--- lib/Db/ClusterMapperTraits/ClusterCountTrait.php
+++ lib/Db/ClusterMapperTraits/ClusterCountTrait.php
@@ -19,7 +19,7 @@ trait ClusterCountTrait
         try {
             $count = count($this->findDistinctNames($userId, $modelId));
 
-            $this->logDebug('Count completed', [
+            $this->logInfo('Count completed', [
                 'userId' => $userId,
                 'modelId' => $modelId,
                 'count' => $count
@@ -74,7 +74,7 @@ trait ClusterCountTrait
 
             $count = $data !== false ? (int)$data[0] : 0;
 
-            $this->logDebug('Count completed', [
+            $this->logInfo('Count completed', [
                 'userId' => $userId,
                 'modelId' => $modelId,
                 'onlyInvalid' => $onlyInvalid,
